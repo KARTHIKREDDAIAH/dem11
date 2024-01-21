4965f741f79fc9e4d04eec44f71dc2efc638abbd
Chinni <Chinni@DESKTOP-JUICAAD>
2024-01-21 09:13:20 +0530

    test commit

---
+++ dem11/src/main/java/com/abhi/introtospringboot/Dem11Application.java
@@ -0,0 +1,14 @@
+package com.abhi.introtospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dem11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dem11Application.class, args);
+		System.out.println("hjjkjfj");
+	}
+
+}
